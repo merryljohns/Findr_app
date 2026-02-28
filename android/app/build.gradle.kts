cd06fb9064e45f554370ee257146df0fd783065e
merryljohns <merryljohns2006@gmail.com>
2026-02-28 06:20:56 +0530

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,11 +6,13 @@ plugins {
 }
 
 android {
+    ndkVersion = "27.0.12077973"
+
     namespace = "com.example.findr_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
 
-    compileOptions {
+    compileOptions {  
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
